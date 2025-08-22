d1aea281acf379f4f2b60442ce0787c8348934c0
DESKTOP-EI0J2HR\admin <Email: nikhil.jangid1@dotsquares.com>
2025-08-22 14:26:07 +0530

    Initial commit - Wallet Payment package

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,9 +6,11 @@ use admin\wallets\Controllers\Admin\WalletWithdrawController;
 
 Route::name('admin.')->middleware(['web','admin.auth'])->group(function () {  
     // Withdraw Request Routes
-    Route::resource('withdraws', WalletWithdrawController::class);
+    Route::resource('withdraws', WalletWithdrawController::class)->only([
+        'index', 'show'
+    ]);
     Route::post('withdraws/{id}/status', [WalletWithdrawController::class, 'changeWithdrawStatus'])
-        ->name('withdraws.changeStatus');
+        ->name('admin.withdraws.changeStatus');
 
     // Transaction History Routes
     Route::resource('transactions', WalletTransactionController::class);
